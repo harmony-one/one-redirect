b33ed3eb8a4d6c305f2ed2927da1ad74181f125d
abhi5456 <abhimamidipaka@gmail.com>
2023-02-21 15:47:41 -0800

    edit popup

--- src/popup/components/app.tsx
+++ src/popup/components/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div id="app-root"><div><h2>Checkout Our Recent Developments:</h2></div> <div><a href="https://videos.0.country" target="_blank"><h3>Video App</h3></a></div><div><a href="https://gatedupload.0.country" target="_blank"><h3>Gated Upload</h3></a></div><div><a href="https://live.0.country" target="_blank"><h3>Live Video</h3></a></div> <div><a href="https://forms.gle/cGnKQxSkHZXMDwFC6" target="_blank"><h4>Feedback? New Feature Suggestion?</h4></a></div><br></br></div>;
+  return <div id="app-root"><div><h2>Checkout Our Recent Developments:</h2></div> <div><a href="https://1.country" target="_blank"><h3>Domains</h3></a></div><div><a href="https://live.0.country" target="_blank"><h3>Live Video</h3></a></div> <div><a href="https://forms.gle/cGnKQxSkHZXMDwFC6" target="_blank"><h4>Feedback? New Feature Suggestion?</h4></a></div><br></br></div>;
 }
 
 export default App;
